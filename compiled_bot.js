4d877722e77e3065378711eab01948de2cb0aea5
cvxluo <cvxluo@users.noreply.github.com>
2019-01-20 17:38:32 -0500

    Tiny change to preachers to make them walk at the enemy if they can't shoot, should help with getting stuck

--- compiled_bot.js
+++ compiled_bot.js
@@ -1,17 +1,577 @@
-import {BCAbstractRobot, SPECS} from 'battlecode';
+'use strict';
 
-import getPath from './astarPath.js'
+var SPECS = {"COMMUNICATION_BITS":16,"CASTLE_TALK_BITS":8,"MAX_ROUNDS":1000,"TRICKLE_FUEL":25,"INITIAL_KARBONITE":100,"INITIAL_FUEL":500,"MINE_FUEL_COST":1,"KARBONITE_YIELD":2,"FUEL_YIELD":10,"MAX_TRADE":1024,"MAX_BOARD_SIZE":64,"MAX_ID":4096,"CASTLE":0,"CHURCH":1,"PILGRIM":2,"CRUSADER":3,"PROPHET":4,"PREACHER":5,"RED":0,"BLUE":1,"CHESS_INITIAL":100,"CHESS_EXTRA":20,"TURN_MAX_TIME":200,"MAX_MEMORY":50000000,"UNITS":[{"CONSTRUCTION_KARBONITE":null,"CONSTRUCTION_FUEL":null,"KARBONITE_CAPACITY":null,"FUEL_CAPACITY":null,"SPEED":0,"FUEL_PER_MOVE":null,"STARTING_HP":200,"VISION_RADIUS":100,"ATTACK_DAMAGE":10,"ATTACK_RADIUS":[1,64],"ATTACK_FUEL_COST":10,"DAMAGE_SPREAD":0},{"CONSTRUCTION_KARBONITE":50,"CONSTRUCTION_FUEL":200,"KARBONITE_CAPACITY":null,"FUEL_CAPACITY":null,"SPEED":0,"FUEL_PER_MOVE":null,"STARTING_HP":100,"VISION_RADIUS":100,"ATTACK_DAMAGE":0,"ATTACK_RADIUS":0,"ATTACK_FUEL_COST":0,"DAMAGE_SPREAD":0},{"CONSTRUCTION_KARBONITE":10,"CONSTRUCTION_FUEL":50,"KARBONITE_CAPACITY":20,"FUEL_CAPACITY":100,"SPEED":4,"FUEL_PER_MOVE":1,"STARTING_HP":10,"VISION_RADIUS":100,"ATTACK_DAMAGE":null,"ATTACK_RADIUS":null,"ATTACK_FUEL_COST":null,"DAMAGE_SPREAD":null},{"CONSTRUCTION_KARBONITE":15,"CONSTRUCTION_FUEL":50,"KARBONITE_CAPACITY":20,"FUEL_CAPACITY":100,"SPEED":9,"FUEL_PER_MOVE":1,"STARTING_HP":40,"VISION_RADIUS":49,"ATTACK_DAMAGE":10,"ATTACK_RADIUS":[1,16],"ATTACK_FUEL_COST":10,"DAMAGE_SPREAD":0},{"CONSTRUCTION_KARBONITE":25,"CONSTRUCTION_FUEL":50,"KARBONITE_CAPACITY":20,"FUEL_CAPACITY":100,"SPEED":4,"FUEL_PER_MOVE":2,"STARTING_HP":20,"VISION_RADIUS":64,"ATTACK_DAMAGE":10,"ATTACK_RADIUS":[16,64],"ATTACK_FUEL_COST":25,"DAMAGE_SPREAD":0},{"CONSTRUCTION_KARBONITE":30,"CONSTRUCTION_FUEL":50,"KARBONITE_CAPACITY":20,"FUEL_CAPACITY":100,"SPEED":4,"FUEL_PER_MOVE":3,"STARTING_HP":60,"VISION_RADIUS":16,"ATTACK_DAMAGE":20,"ATTACK_RADIUS":[1,16],"ATTACK_FUEL_COST":15,"DAMAGE_SPREAD":3}]};
 
-import {
-  buildAround,
-  findNearestPath,
-  overlapPassableMaps,
-  overlapResourceMaps,
-  passableMapsNoResources,
-  getSymmetry,
-  countResources,
-  makeMapCopy
- } from './utils.js'
+function insulate(content) {
+    return JSON.parse(JSON.stringify(content));
+}
+
+class BCAbstractRobot {
+    constructor() {
+        this._bc_reset_state();
+    }
+
+    // Hook called by runtime, sets state and calls turn.
+    _do_turn(game_state) {
+        this._bc_game_state = game_state;
+        this.id = game_state.id;
+        this.karbonite = game_state.karbonite;
+        this.fuel = game_state.fuel;
+        this.last_offer = game_state.last_offer;
+
+        this.me = this.getRobot(this.id);
+
+        if (this.me.turn === 1) {
+            this.map = game_state.map;
+            this.karbonite_map = game_state.karbonite_map;
+            this.fuel_map = game_state.fuel_map;
+        }
+
+        try {
+            var t = this.turn();
+        } catch (e) {
+            t = this._bc_error_action(e);
+        }
+
+        if (!t) t = this._bc_null_action();
+
+        t.signal = this._bc_signal;
+        t.signal_radius = this._bc_signal_radius;
+        t.logs = this._bc_logs;
+        t.castle_talk = this._bc_castle_talk;
+
+        this._bc_reset_state();
+
+        return t;
+    }
+
+    _bc_reset_state() {
+        // Internal robot state representation
+        this._bc_logs = [];
+        this._bc_signal = 0;
+        this._bc_signal_radius = 0;
+        this._bc_game_state = null;
+        this._bc_castle_talk = 0;
+        this.me = null;
+        this.id = null;
+        this.fuel = null;
+        this.karbonite = null;
+        this.last_offer = null;
+    }
+
+    // Action template
+    _bc_null_action() {
+        return {
+            'signal': this._bc_signal,
+            'signal_radius': this._bc_signal_radius,
+            'logs': this._bc_logs,
+            'castle_talk': this._bc_castle_talk
+        };
+    }
+
+    _bc_error_action(e) {
+        var a = this._bc_null_action();
+        
+        if (e.stack) a.error = e.stack;
+        else a.error = e.toString();
+
+        return a;
+    }
+
+    _bc_action(action, properties) {
+        var a = this._bc_null_action();
+        if (properties) for (var key in properties) { a[key] = properties[key]; }
+        a['action'] = action;
+        return a;
+    }
+
+    _bc_check_on_map(x, y) {
+        return x >= 0 && x < this._bc_game_state.shadow[0].length && y >= 0 && y < this._bc_game_state.shadow.length;
+    }
+    
+    log(message) {
+        this._bc_logs.push(JSON.stringify(message));
+    }
+
+    // Set signal value.
+    signal(value, radius) {
+        // Check if enough fuel to signal, and that valid value.
+        
+        var fuelNeeded = Math.ceil(Math.sqrt(radius));
+        if (this.fuel < fuelNeeded) throw "Not enough fuel to signal given radius.";
+        if (!Number.isInteger(value) || value < 0 || value >= Math.pow(2,SPECS.COMMUNICATION_BITS)) throw "Invalid signal, must be int within bit range.";
+        if (radius > 2*Math.pow(SPECS.MAX_BOARD_SIZE-1,2)) throw "Signal radius is too big.";
+
+        this._bc_signal = value;
+        this._bc_signal_radius = radius;
+
+        this.fuel -= fuelNeeded;
+    }
+
+    // Set castle talk value.
+    castleTalk(value) {
+        // Check if enough fuel to signal, and that valid value.
+
+        if (!Number.isInteger(value) || value < 0 || value >= Math.pow(2,SPECS.CASTLE_TALK_BITS)) throw "Invalid castle talk, must be between 0 and 2^8.";
+
+        this._bc_castle_talk = value;
+    }
+
+    proposeTrade(karbonite, fuel) {
+        if (this.me.unit !== SPECS.CASTLE) throw "Only castles can trade.";
+        if (!Number.isInteger(karbonite) || !Number.isInteger(fuel)) throw "Must propose integer valued trade."
+        if (Math.abs(karbonite) >= SPECS.MAX_TRADE || Math.abs(fuel) >= SPECS.MAX_TRADE) throw "Cannot trade over " + SPECS.MAX_TRADE + " in a given turn.";
+
+        return this._bc_action('trade', {
+            trade_fuel: fuel,
+            trade_karbonite: karbonite
+        });
+    }
+
+    buildUnit(unit, dx, dy) {
+        if (this.me.unit !== SPECS.PILGRIM && this.me.unit !== SPECS.CASTLE && this.me.unit !== SPECS.CHURCH) throw "This unit type cannot build.";
+        if (this.me.unit === SPECS.PILGRIM && unit !== SPECS.CHURCH) throw "Pilgrims can only build churches.";
+        if (this.me.unit !== SPECS.PILGRIM && unit === SPECS.CHURCH) throw "Only pilgrims can build churches.";
+        
+        if (!Number.isInteger(dx) || !Number.isInteger(dx) || dx < -1 || dy < -1 || dx > 1 || dy > 1) throw "Can only build in adjacent squares.";
+        if (!this._bc_check_on_map(this.me.x+dx,this.me.y+dy)) throw "Can't build units off of map.";
+        if (this._bc_game_state.shadow[this.me.y+dy][this.me.x+dx] > 0) throw "Cannot build on occupied tile.";
+        if (!this.map[this.me.y+dy][this.me.x+dx]) throw "Cannot build onto impassable terrain.";
+        if (this.karbonite < SPECS.UNITS[unit].CONSTRUCTION_KARBONITE || this.fuel < SPECS.UNITS[unit].CONSTRUCTION_FUEL) throw "Cannot afford to build specified unit.";
+
+        return this._bc_action('build', {
+            dx: dx, dy: dy,
+            build_unit: unit
+        });
+    }
+
+    move(dx, dy) {
+        if (this.me.unit === SPECS.CASTLE || this.me.unit === SPECS.CHURCH) throw "Churches and Castles cannot move.";
+        if (!this._bc_check_on_map(this.me.x+dx,this.me.y+dy)) throw "Can't move off of map.";
+        if (this._bc_game_state.shadow[this.me.y+dy][this.me.x+dx] === -1) throw "Cannot move outside of vision range.";
+        if (this._bc_game_state.shadow[this.me.y+dy][this.me.x+dx] !== 0) throw "Cannot move onto occupied tile.";
+        if (!this.map[this.me.y+dy][this.me.x+dx]) throw "Cannot move onto impassable terrain.";
+
+        var r = Math.pow(dx,2) + Math.pow(dy,2);  // Squared radius
+        if (r > SPECS.UNITS[this.me.unit]['SPEED']) throw "Slow down, cowboy.  Tried to move faster than unit can.";
+        if (this.fuel < r*SPECS.UNITS[this.me.unit]['FUEL_PER_MOVE']) throw "Not enough fuel to move at given speed.";
+
+        return this._bc_action('move', {
+            dx: dx, dy: dy
+        });
+    }
+
+    mine() {
+        if (this.me.unit !== SPECS.PILGRIM) throw "Only Pilgrims can mine.";
+        if (this.fuel < SPECS.MINE_FUEL_COST) throw "Not enough fuel to mine.";
+        
+        if (this.karbonite_map[this.me.y][this.me.x]) {
+            if (this.me.karbonite >= SPECS.UNITS[SPECS.PILGRIM].KARBONITE_CAPACITY) throw "Cannot mine, as at karbonite capacity.";
+        } else if (this.fuel_map[this.me.y][this.me.x]) {
+            if (this.me.fuel >= SPECS.UNITS[SPECS.PILGRIM].FUEL_CAPACITY) throw "Cannot mine, as at fuel capacity.";
+        } else throw "Cannot mine square without fuel or karbonite.";
+
+        return this._bc_action('mine');
+    }
+
+    give(dx, dy, karbonite, fuel) {
+        if (dx > 1 || dx < -1 || dy > 1 || dy < -1 || (dx === 0 && dy === 0)) throw "Can only give to adjacent squares.";
+        if (!this._bc_check_on_map(this.me.x+dx,this.me.y+dy)) throw "Can't give off of map.";
+        if (this._bc_game_state.shadow[this.me.y+dy][this.me.x+dx] <= 0) throw "Cannot give to empty square.";
+        if (karbonite < 0 || fuel < 0 || this.me.karbonite < karbonite || this.me.fuel < fuel) throw "Do not have specified amount to give.";
+
+        return this._bc_action('give', {
+            dx:dx, dy:dy,
+            give_karbonite:karbonite,
+            give_fuel:fuel
+        });
+    }
+
+    attack(dx, dy) {
+        if (this.me.unit === SPECS.CHURCH) throw "Churches cannot attack.";
+        if (this.fuel < SPECS.UNITS[this.me.unit].ATTACK_FUEL_COST) throw "Not enough fuel to attack.";
+        if (!this._bc_check_on_map(this.me.x+dx,this.me.y+dy)) throw "Can't attack off of map.";
+        if (this._bc_game_state.shadow[this.me.y+dy][this.me.x+dx] === -1) throw "Cannot attack outside of vision range.";
+
+        var r = Math.pow(dx,2) + Math.pow(dy,2);
+        if (r > SPECS.UNITS[this.me.unit]['ATTACK_RADIUS'][1] || r < SPECS.UNITS[this.me.unit]['ATTACK_RADIUS'][0]) throw "Cannot attack outside of attack range.";
+
+        return this._bc_action('attack', {
+            dx:dx, dy:dy
+        });
+        
+    }
+
+
+    // Get robot of a given ID
+    getRobot(id) {
+        if (id <= 0) return null;
+        for (var i=0; i<this._bc_game_state.visible.length; i++) {
+            if (this._bc_game_state.visible[i].id === id) {
+                return insulate(this._bc_game_state.visible[i]);
+            }
+        } return null;
+    }
+
+    // Check if a given robot is visible.
+    isVisible(robot) {
+        return ('unit' in robot);
+    }
+
+    // Check if a given robot is sending you radio.
+    isRadioing(robot) {
+        return robot.signal >= 0;
+    }
+
+    // Get map of visible robot IDs.
+    getVisibleRobotMap() {
+        return this._bc_game_state.shadow;
+    }
+
+    // Get boolean map of passable terrain.
+    getPassableMap() {
+        return this.map;
+    }
+
+    // Get boolean map of karbonite points.
+    getKarboniteMap() {
+        return this.karbonite_map;
+    }
+
+    // Get boolean map of impassable terrain.
+    getFuelMap() {
+        return this.fuel_map;
+    }
+
+    // Get a list of robots visible to you.
+    getVisibleRobots() {
+        return this._bc_game_state.visible;
+    }
+
+    turn() {
+        return null;
+    }
+}
+
+class Tile {
+  constructor(x, y, distance, previousTile) {
+    this.x = x;
+    this.y = y;
+    this.distance = distance;
+    this.previousTile = previousTile;
+  }
+
+  getXY() {
+    return [this.x, this.y];
+  }
+
+  getNeighbors(map) {
+    var tiles = [];
+    for (var x = -1; x < 2; x++) {
+      for (var y = -1; y < 2; y++) {
+        const dX = x + this.x;
+        const dY = y + this.y;
+        if ((dX >= 0 && dX < map.length && dY >= 0 && dY < map.length) && map[dY][dX]) {
+          if (x != 0 || y != 0) {
+            var tile = new Tile(dX, dY, this.distance + 1, this);
+            tiles.push(tile);
+          }
+          map[dY][dX] = false;
+        }
+      }
+    }
+
+    return [tiles, map];
+  }
+
+  getMovement(other) {
+    var dX = other.x - this.x;
+    var dY = other.y - this.y;
+    if (dX != 0) dX /= Math.abs(dX);
+    if (dY != 0) dY /= Math.abs(dY);
+
+    return [dX, dY];
+  }
+
+  toString() {
+    return "x: " + this.x + " y: " + this.y;
+  }
+
+}
+
+// https://github.com/mourner/tinyqueue
+
+class MinPQ {
+    constructor(data = [], compare = defaultCompare) {
+        this.data = data;
+        this.length = this.data.length;
+        this.compare = compare;
+
+        if (this.length > 0) {
+            for (let i = (this.length >> 1) - 1; i >= 0; i--) this._down(i);
+        }
+    }
+
+    push(item) {
+        this.data.push(item);
+        this.length++;
+        this._up(this.length - 1);
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        const top = this.data[0];
+        const bottom = this.data.pop();
+        this.length--;
+
+        if (this.length > 0) {
+            this.data[0] = bottom;
+            this._down(0);
+        }
+
+        return top;
+    }
+
+    peek() {
+        return this.data[0];
+    }
+
+    size() {
+      return this.length;
+    }
+
+    _up(pos) {
+        const {data, compare} = this;
+        const item = data[pos];
+
+        while (pos > 0) {
+            const parent = (pos - 1) >> 1;
+            const current = data[parent];
+            if (compare(item, current) >= 0) break;
+            data[pos] = current;
+            pos = parent;
+        }
+
+        data[pos] = item;
+    }
+
+    _down(pos) {
+        const {data, compare} = this;
+        const halfLength = this.length >> 1;
+        const item = data[pos];
+
+        while (pos < halfLength) {
+            let left = (pos << 1) + 1;
+            let best = data[left];
+            const right = left + 1;
+
+            if (right < this.length && compare(data[right], best) < 0) {
+                left = right;
+                best = data[right];
+            }
+            if (compare(best, item) >= 0) break;
+
+            data[pos] = best;
+            pos = left;
+        }
+
+        data[pos] = item;
+    }
+}
+
+function defaultCompare(a, b) {
+    return a.distance < b.distance ? -1 : a.distance > b.distance ? 1 : 0;
+}
+
+function getPath(loc, dest, passMap) {
+  var pathMap = [];
+  for (var x = 0; x < passMap.length; x++) {
+      pathMap[x] = passMap[x].slice();
+  }
+
+  const destX = dest[0];
+  const destY = dest[1];
+
+  if (!passMap[destY][destX]) {
+    pathMap[destY][destX] = true;
+  }
+
+  var distance = 0;
+
+  var minPQ = new MinPQ();
+
+  var tile = new Tile(loc[0], loc[1], distance, null);
+  minPQ.push(tile);
+  passMap[loc[1]][loc[0]] = false;
+
+  while (minPQ.size() != 0 && (minPQ.peek().x != destX || minPQ.peek().y != destY)) {
+    var minTile = minPQ.pop();
+    var neighbors = minTile.getNeighbors(pathMap);
+    for (var i = 0; i < neighbors[0].length; i++) {
+      minPQ.push(neighbors[0][i]);
+    }
+    pathMap = neighbors[1];
+  }
+
+  var path = [];
+  var pathTile = minPQ.peek();
+
+  while (pathTile != null) {
+    path.unshift(pathTile);
+    pathTile = pathTile.previousTile;
+  }
+
+  /*
+  for (var i = 0; i < path.length; i++) {
+    this.log(path[i].toString());
+  }
+  */
+
+  return path;
+}
+
+function buildAround(loc, fullPassMap) {
+  for (var x = -1; x < 2; x++) {
+    for (var y = -1; y < 2; y++) {
+      const dX = x + loc[0];
+      const dY = y + loc[1];
+      if ((dX >= 0 && dX < fullPassMap.length && dY >= 0 && dY < fullPassMap.length) && fullPassMap[dY][dX]) {
+        return [x, y];
+      }
+    }
+  }
+  return [0, 0];
+}
+
+function findNearestPath(location, resourceMap, passMap, excludes) {
+
+  var resourceCopy = [];
+  for (var x = 0; x < resourceMap.length; x++) {
+      resourceCopy[x] = resourceMap[x].slice();
+  }
+
+  for (var i = 0; i < excludes.length; i++) {
+    const exclude = excludes[i];
+    resourceCopy[exclude[1]][exclude[0]] = false;
+  }
+
+  /*
+  for (var x = 0; x < resourceCopy.length; x++) {
+    for (var y = 0; y < resourceCopy[x].length; y++) {
+      if (resourceCopy[y][x] && excludes.includes([x, y])) {
+        resourceCopy[y][x] = false;
+      }
+    }
+  }
+  */
+
+  const loc = [location[0], location[1]];
+  var paths = [];
+
+  for (var x = 0; x < resourceCopy.length; x++) {
+    for (var y = 0; y < resourceCopy[x].length; y++) {
+      if (resourceCopy[y][x]) {
+        const d = [x, y];
+        const path = getPath(location, d, passMap);
+        //this.log(path);
+        paths.push(path);
+      }
+    }
+  }
+
+  // Always will be at least 1 node for resources
+  var min = paths[0];
+  for (var i = 0; i < paths.length; i++) {
+    if (paths[i].length <= min.length && paths[i].length != 0) { min = paths[i]; }
+  }
+
+  // TODO: Sometimes an empty path is being returned
+  return min;
+
+}
+
+function overlapPassableMaps(passable, robot) {
+  var newMap = makeMapCopy(passable);
+
+  for (var x = 0; x < robot.length; x++) {
+    for (var y = 0; y < robot[x].length; y++) {
+        if (newMap[y][x] && robot[y][x] > 0) newMap[y][x] = false;
+    }
+  }
+
+  return newMap;
+
+}
+
+function overlapResourceMaps(resource1, resource2) {
+  var newMap = makeMapCopy(resource1);
+
+  for (var x = 0; x < resource2.length; x++) {
+    for (var y = 0; y < resource2[x].length; y++) {
+        if (resource2[y][x]) newMap[y][x] = true;
+    }
+  }
+
+  return newMap;
+
+}
+
+function passableMapsNoResources(passable, robot, resource1, resource2) {
+  const passMap = overlapPassableMaps(passable, robot);
+  const resources = overlapResourceMaps(resource1, resource2);
+
+  var newMap = makeMapCopy(passMap);
+
+  for (var x = 0; x < resources.length; x++) {
+    for (var y = 0; y < resources[x].length; y++) {
+        if (resources[y][x]) newMap[y][x] = false;
+    }
+  }
+
+  return newMap;
+
+}
+
+// Returns false if horizontally symmetric, true if vertically
+function getSymmetry(resourceMap) {
+  for (var x = 0; x < resourceMap.length; x++) {
+    for (var y = 0; y < resourceMap[x].length; y++) {
+      if (resourceMap[y][x]) {
+
+        if (resourceMap[(resourceMap.length - y - 1)][x]) {
+          return true;
+        }
+        else {
+          return false;
+        }
+
+      }
+    }
+  }
+}
+
+function countResources(resourceMap) {
+  var count = 0;
+  for (var x = 0; x < resourceMap.length; x++) {
+    for (var y = 0; y < resourceMap[x].length; y++) {
+      if (resourceMap[y][x]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function makeMapCopy(toCopy) {
+
+  var newMap = [];
+  for (var x = 0; x < toCopy.length; x++) {
+      newMap.push(toCopy[x].slice());
+  }
+
+  return newMap;
+}
 
 // bc19run -b javascript -r javascript --replay "replay.bc19" --seed 1
 // --chi 1000
@@ -784,5 +1344,7 @@ class MyRobot extends BCAbstractRobot {
     }
 
 }
+
+var robot = new MyRobot();
 
 var robot = new MyRobot();
